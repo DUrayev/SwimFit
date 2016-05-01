aa3e41a13b3b47a5b7c5d989ce4b0cddd698b272
Denis Urayev <DenisUraev@gmail.com>
2016-05-01 23:23:43 +0300

    charts added

--- platforms/android/assets/www/js/controllers.js
+++ platforms/android/assets/www/js/controllers.js
@@ -5,27 +5,37 @@
 var swimFitControllers = angular.module('swimFitControllers', []);
 
 swimFitControllers.controller('OverviewCtrl', ['$scope', 'TrainingRecordsService',
-  function($scope, TrainingRecordsService) {
-  	$scope.message = 'Hello world';
+  function($scope, TrainingRecordsService, $ionicView) {
     $scope.loading = true;
-    $scope.statistics = {
-      thisDay: 0,
-      totalDistance: 0,
-      totalThisMonthDistance: 0,
-      totalThisWeekDistance: 0,
-      totalLastMonthDistance: 0,
-      totalLastWeekDistance: 0,
-      averageInDay: 0,
-      averageInMonth: 0
-    };
-    $scope.bar = {
-      labels: [],
-      data: [],
-      series: ['Total distance']
-    };
-    TrainingRecordsService.getTrainings().then(function(result) {
+
+    
+
+    $scope.$on('$ionicView.enter', function(){
+      $scope.loading = true;
+      $scope.statistics = {
+        thisDay: 0,
+        totalDistance: 0,
+        totalThisMonthDistance: 0,
+        totalThisWeekDistance: 0,
+        totalLastMonthDistance: 0,
+        totalLastWeekDistance: 0,
+        averageInDay: 0,
+        averageInMonth: 0
+      };
+      $scope.bar = {
+        labels: [],
+        data: [[]],
+        series: ['Total distance']
+      };
+      $scope.doughnut = {
+        labels: [],
+        data: []
+      };
+      TrainingRecordsService.getTrainings().then(function(result) {
         var groupedByDay = {};
         var groupedByMonth = {};
+        var groupedByWeek = {};
+
         for(var i = 0; i < result.length; i++) {
           $scope.statistics.totalDistance += +result[i].distance;
 
@@ -34,6 +44,9 @@ swimFitControllers.controller('OverviewCtrl', ['$scope', 'TrainingRecordsService
 
           groupedByMonth[moment(+result[i].timestamp).format('MMM YYYY')] = groupedByMonth[moment(+result[i].timestamp).format('MMMM YYYY')] || 0;
           groupedByMonth[moment(+result[i].timestamp).format('MMM YYYY')] += +result[i].distance;
+
+          groupedByWeek[moment(+result[i].timestamp).format('dddd')] = groupedByMonth[moment(+result[i].timestamp).format('dddd')] || 0;
+          groupedByWeek[moment(+result[i].timestamp).format('dddd')] += +result[i].distance;
 
           if(moment(+result[i].timestamp).isSame(moment(), 'day')) {
             $scope.statistics.thisDay += +result[i].distance;
@@ -57,16 +70,23 @@ swimFitControllers.controller('OverviewCtrl', ['$scope', 'TrainingRecordsService
         for(var i = 0; i < 6; i++) { //last 6 months bar chart data
           var monthLabel = moment().subtract(5 - i, 'months').format('MMM YYYY');
           $scope.bar.labels.push(monthLabel);
-          $scope.bar.data.push(groupedByMonth[monthLabel] || 0);          
+          $scope.bar.data[0].push(groupedByMonth[monthLabel] || 0);          
         }
+        for(var i = 1; i <= 7; i++) {
+          var weekLabel = moment().isoWeekday(i).format('dddd');
+          $scope.doughnut.labels.push(weekLabel);
+          $scope.doughnut.data.push(groupedByWeek[weekLabel] || 0);  
+        }
+        
 
         $scope.loading = false;
-    }, function(error) {
-      $scope.loading = false;
-      alert(error);
+      }, function(error) {
+        $scope.loading = false;
+        alert(error);
+      });
     });
+    
   	$scope.slideHasChanged = function(index) {
-  		$scope.index = index;
   	}
   }
 ]);
@@ -128,7 +148,6 @@ swimFitControllers.controller('DashboardCtrl', ['$scope', 'TrainingRecordsServic
 
 swimFitControllers.controller('AddCtrl', ['$scope', '$ionicActionSheet', 'TrainingRecordsService', '$cordovaSQLite',
   function($scope, $ionicActionSheet, TrainingRecordsService, $cordovaSQLite) {
-    $scope.isChecked = true;
     $scope.clearForm = function() {
       $scope.currentDate = new Date();
       $scope.minDate = new Date(2010, 6, 1);
